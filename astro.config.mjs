be4738c7db5b70a7039ed7724ae9ec63cc77288b
Bright Larson <emehado@gmail.com>
2024-01-13 17:42:59 +0000

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import htmx from "astro-htmx";
 import node from "@astrojs/node";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://emehado.github.io",
   base: "/astro-htmx",
   integrations: [tailwind(), htmx()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
 });
